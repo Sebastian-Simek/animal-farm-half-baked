e4a9e04fd72921ac3c547a7d970e2de1db49f7d5
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-09 12:29:47 -0700

    minimum turn-in standards hit

--- animal-facts/animal-facts.js
+++ animal-facts/animal-facts.js
@@ -1,6 +1,12 @@
-import { findById } from "../utils.js";
-import { animals } from "../data.js";
+import { findById } from '../utils.js';
+import { animals } from '../data.js';
+import { renderListItemDetail } from '../render-utils.js';
 
 const params = new URLSearchParams(window.location.search);
+const main = document.querySelector('main');
 
 const animal = findById(params.get('id'), animals);
+const animalDiv = renderListItemDetail(animal);
+main.append(animalDiv);
+
+
--- render-utils.js
+++ render-utils.js
@@ -5,7 +5,7 @@ export function renderListItem(animal) {
     div.style.left = animal.left;
 
     const img = document.createElement('img');
-    img.src = `./assets/${animal.type}.svg`;
+    img.src = `../assets/${animal.type}.svg`;
 
     const nameSpan = document.createElement('span');
     nameSpan.textContent = animal.name;
@@ -21,3 +21,27 @@ export function renderListItem(animal) {
     div.append(img, nameSpan, span, a);
     return div;
 }
+
+export function renderListItemDetail(animal) {
+    const div = document.createElement('div');
+    div.classList.add('animal');
+    div.style.top = animal.top;
+    div.style.left = animal.left;
+
+    const h2 = document.createElement('h2');
+    h2.textContent = `Info about ${animal.name}`;
+
+    const img = document.createElement('img');
+    img.src = `../assets/${animal.type}.svg`;
+
+    const nameSpan = document.createElement('span');
+    nameSpan.textContent = animal.name;
+    nameSpan.classList.add('name');
+
+    const span = document.createElement('span');
+    span.textContent = animal.says;
+
+    div.append(h2, img, nameSpan, span,);
+    return div;
+}
+
